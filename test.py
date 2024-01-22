20195dd6e6f77ab8f3d44858a11cfd4cb664980d
MiLL4U <aphiloboe@gmail.com>
2024-01-22 16:35:16 +0900

    add test script for basic HCA

---
+++ test.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from hshca import HierarchicalClusterAnalysis
+from hshca.linkmethod import Centroid
+from hshca.metric import Euclidean
+
+DATA_PATH = "./rand.npy"
+METHOD = Centroid
+METRIC = Euclidean
+
+data = np.array(np.load(DATA_PATH))  # 3D array (x, y, r)
+print(data)
+print()
+
+vec_num = data.shape[0] * data.shape[1]
+data_2d = data.reshape(vec_num, data.shape[2])  # (i, r)
+print(data_2d)
+
+hca = HierarchicalClusterAnalysis(data_2d, METHOD, METRIC)
+hca.compute()
+print(hca.distance_matrix)
