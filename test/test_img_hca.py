803feb8595b9cd82b4982d7c0f6e2647c966dd51
MiLL4U <aphiloboe@gmail.com>
2024-01-31 13:02:24 +0900

    save result of test scripts to dst directory

--- test/test_img_hca.py
+++ test/test_img_hca.py
@@ -1,11 +1,14 @@
+from os import makedirs
+
 import ibwpy as ip
 import matplotlib.pyplot as plt
 
 from hshca import MultiDimensionalHCA
 from hshca.linkmethod import Centroid, Ward  # noqa
-from hshca.metric import Euclidean, CityBlock  # noqa
+from hshca.metric import CityBlock, Euclidean  # noqa
 
 DATA_PATH = "./test/mHeLa_control_1_smth.ibw"
+DST_PATH = "./dst/"
 MAP_SHAPE = (30, 30)
 
 METHOD = Ward
@@ -14,6 +17,8 @@ SPATIAL_DIST_FACTOR = 0.0003
 SPATIAL_SCALE = (1.0, 1.0, 1.0)
 
 CLUSTER_NUMS = (3, 4, 5, 6, 7, 8)
+
+makedirs(DST_PATH, exist_ok=True)
 
 ibw = ip.load(DATA_PATH)
 data = ibw.array  # 4D array (x, y, z, r)
@@ -28,4 +33,4 @@ for cluster_num in CLUSTER_NUMS:
     plt.imshow(res)
 
     name = f"cls{cluster_num}_normal.png"
-    plt.savefig(name)
+    plt.savefig(DST_PATH + name)
--- test/test_img_multi.py
+++ test/test_img_multi.py
@@ -1,3 +1,5 @@
+from os import makedirs
+
 import ibwpy as ip
 import matplotlib.pyplot as plt
 
@@ -6,6 +8,7 @@ from hshca.linkmethod import Centroid, Ward  # noqa
 from hshca.metric import CityBlock, Euclidean  # noqa
 
 DATA_PATH = "./test/mHeLa_control_1_smth.ibw"
+DST_PATH = "./dst/"
 MAP_SHAPE = (30, 30)
 
 METHOD = Ward
@@ -15,6 +18,8 @@ SPATIAL_FACTORS = (1e-7, 5e-7, 1e-6, 5e-6, 1e-5,
 SPATIAL_SCALE = (1.0, 1.0, 1.0)
 
 CLUSTER_NUMS = (3, 4, 5, 6, 7, 8)
+
+makedirs(DST_PATH, exist_ok=True)
 
 ibw = ip.load(DATA_PATH)
 data = ibw.array  # 4D array (x, y, z, r)
@@ -33,4 +38,4 @@ for factor_idx, factor in enumerate(SPATIAL_FACTORS):
         plt.imshow(res)
 
         name = f"cls{cluster_num}_{factor_idx}_{factor:.0e}.png"
-        plt.savefig(name)
+        plt.savefig(DST_PATH + name)
