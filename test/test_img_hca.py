0c119df88f9fe00606d28d13dbf6ee71fcef426e
MiLL4U <aphiloboe@gmail.com>
2024-01-31 13:14:35 +0900

    use npy file in test scripts (not ibw)

--- test/test_img_hca.py
+++ test/test_img_hca.py
@@ -1,15 +1,14 @@
 from os import makedirs
 
-import ibwpy as ip
 import matplotlib.pyplot as plt
+import numpy as np
 
 from hshca import MultiDimensionalHCA
 from hshca.linkmethod import Centroid, Ward  # noqa
 from hshca.metric import CityBlock, Euclidean  # noqa
 
-DATA_PATH = "./test/mHeLa_control_1_smth.ibw"
+DATA_PATH = "./test/test_cell.npy"
 DST_PATH = "./dst/"
-MAP_SHAPE = (30, 30)
 
 METHOD = Ward
 METRIC = Euclidean
@@ -19,17 +18,14 @@ SPATIAL_SCALE = (1.0, 1.0, 1.0)
 CLUSTER_NUMS = (3, 4, 5, 6, 7, 8)
 
 makedirs(DST_PATH, exist_ok=True)
-
-ibw = ip.load(DATA_PATH)
-data = ibw.array  # 4D array (x, y, z, r)
-# print(data)
+data = np.load(DATA_PATH)  # 3D array (x, y, r)
 
 hca = MultiDimensionalHCA(
     data, METHOD, METRIC, show_progress=True)
 hca.compute()
 
 for cluster_num in CLUSTER_NUMS:
-    res = hca.get_cluster_map(cluster_num).reshape(MAP_SHAPE).T
+    res = hca.get_cluster_map(cluster_num).reshape(hca.map_shape)
     plt.imshow(res)
 
     name = f"cls{cluster_num}_normal.png"
--- test/test_img_multi.py
+++ test/test_img_multi.py
@@ -1,28 +1,26 @@
 from os import makedirs
 
-import ibwpy as ip
 import matplotlib.pyplot as plt
+import numpy as np
 
 from hshca import HyperSpectralHCA
 from hshca.linkmethod import Centroid, Ward  # noqa
 from hshca.metric import CityBlock, Euclidean  # noqa
 
-DATA_PATH = "./test/mHeLa_control_1_smth.ibw"
+DATA_PATH = "./test/test_cell.npy"
 DST_PATH = "./dst/"
-MAP_SHAPE = (30, 30)
 
 METHOD = Ward
 METRIC = Euclidean
 SPATIAL_FACTORS = (1e-7, 5e-7, 1e-6, 5e-6, 1e-5,
                    1e-4, 3e-4, 5e-4, 8e-4, 3e-3, 3e-2)
-SPATIAL_SCALE = (1.0, 1.0, 1.0)
+SPATIAL_SCALE = (1.0, 1.0)
 
 CLUSTER_NUMS = (3, 4, 5, 6, 7, 8)
 
 makedirs(DST_PATH, exist_ok=True)
 
-ibw = ip.load(DATA_PATH)
-data = ibw.array  # 4D array (x, y, z, r)
+data = np.load(DATA_PATH)  # 3D array (x, y, r)
 
 
 for factor_idx, factor in enumerate(SPATIAL_FACTORS):
@@ -34,7 +32,7 @@ for factor_idx, factor in enumerate(SPATIAL_FACTORS):
     hca.compute()
 
     for cluster_num in CLUSTER_NUMS:
-        res = hca.get_cluster_map(cluster_num).reshape(MAP_SHAPE).T
+        res = hca.get_cluster_map(cluster_num).reshape(hca.map_shape)
         plt.imshow(res)
 
         name = f"cls{cluster_num}_{factor_idx}_{factor:.0e}.png"
